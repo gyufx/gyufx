ba9a88ff9e3ffe038a109858c310c9d68f1c55bf
Gyu <90133511+gyufx@users.noreply.github.com>
2026-04-01 03:27:52 +0900

    update header and main page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'gyufx';
+export const SITE_DESCRIPTION = '기록 및 개발용';
